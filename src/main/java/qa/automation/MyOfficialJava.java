e00e1da2ecb69c9ccf6ed02b5c74c9c8d386f054
Petko Gerdemski <peco20@abv.bg>
2022-01-26 20:30:53 +0200

    Initial commit

---
+++ src/main/java/qa/automation/MyOfficialJava.java
@@ -0,0 +1,37 @@
+package qa.automation;
+
+import java.util.Scanner;
+
+public class MyOfficialJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your name");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+
+        System.out.println("Your name is: " + firstName);
+
+        System.out.println("enter your age:");
+
+        int userAge = scanner.nextInt();
+
+        System.out.println("Your name is: " + firstName +
+                "Your age is: " + userAge);
+
+        if (userAge >= 18) {
+            System.out.println(firstName + " can buy alcohol!!!");
+        }
+
+        if (userAge < 18) {
+            System.out.println(firstName + " can't buy alcohol!!!");
+        }
+        
+        if (userAge >=65) {
+            System.out.println(firstName + " is retired");
+        }
+
+    }
+}
